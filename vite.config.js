d0770d62a3330adbc61e98db67cc508098e3bea5
Hatziry Vitales Herrera <118234336+vhhatziry@users.noreply.github.com>
2025-11-02 22:25:05 -0600

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Asegúrate de cambiar 'mi-cv-web' por el nombre real de tu repositorio
   base: '/mi-cv-web/', 
   plugins: [/* ... */],
 })
